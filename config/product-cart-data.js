e98fbb3abbeddf014441481778cff01ae0e341e2
Ivan Gabriel Guerra <ivanguerra09@gmail.com>
2021-03-09 15:28:46 -0300

    config files

---
+++ config/product-cart-data.js
@@ -0,0 +1,10 @@
+export const productCartItemsData = [
+  {
+    "image_url": "",
+    "model_name": "",
+    "model_gender": "",
+    "model_color": "",
+    "sizes_selected": [],
+    "price": ""
+  }
+];
---
+++ config/product-data.js
@@ -0,0 +1,22 @@
+export const productItemsData = [
+  {
+    "image_urls": {
+      "model_white": "/assets/images/product_color_option_white.svg",
+      "model_black": "/assets/images/product_color_option_black.svg"
+    },
+    "model_name": "Nike Air Force 1 '07",
+    "model_genders": ["Hombre", "Mujer"],
+    "model_colors": ["white", "black"],
+    "sizes_available": [
+      "Cm 24 (Us 6)",
+      "Cm 25",
+      "Cm 26",
+      "Cm 27",
+      "Cm 28",
+      "Cm 29",
+      "Cm 30",
+      "Cm 31"
+    ],
+    "price": 1999
+  }
+];
